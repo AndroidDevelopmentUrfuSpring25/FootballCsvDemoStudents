13684a42309158c1ab7b0152bc648b7fab21a69a
Almazova Vlada <almazova.2004@mail.ru>
2025-02-16 02:27:47 +0500

    реализация классов данных

--- src/main/kotlin/model/Player.kt
+++ src/main/kotlin/model/Player.kt
@@ -1,3 +1,17 @@
 package model
 
-class Player
+/**
+ * Класс данных футболиста
+ */
+class Player (
+    val name: String,
+    val positionOnField: String,
+    val nationality: String,
+    val agency: String,
+    val transferCost: Long,
+    val participations: Int,
+    val goals: Int,
+    val assists: Int,
+    val yellowCards: Int,
+    val redCards: Int
+)
--- src/main/kotlin/model/Team.kt
+++ src/main/kotlin/model/Team.kt
@@ -1,3 +1,22 @@
 package model
 
-class Team
+/**
+ * Класс данных футбольной команды
+ */
+class Team(
+    val name: String,
+    val city: String
+){
+    private var players: MutableSet<Player> = mutableSetOf()
+
+    /**
+     * Добавляет игрока в команду
+     */
+    fun addPlayer(player: Player){
+        players.add(player)
+    }
+
+    fun getPlayers():Set<Player>{
+        return players.toSet()
+    }
+}
