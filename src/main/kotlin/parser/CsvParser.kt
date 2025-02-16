18a33af46844de9ffd2f4da75cd7fd84309ea872
Anna2002k <79574529+Anna2002k@users.noreply.github.com>
2025-02-16 18:43:51 +0500

    Написан код для ответа на вопросы и выполнен 4 вариант задания

--- src/main/kotlin/parser/CsvParser.kt
+++ src/main/kotlin/parser/CsvParser.kt
@@ -1,3 +1,50 @@
 package parser
 
-object CsvParser
+import model.Player
+import model.Position
+import model.Team
+import java.io.File
+
+object CsvParser {
+
+    private var players: MutableList<Player> = mutableListOf()
+
+    fun parse(path: String): List<Player> {
+        players.clear() // Очищаем список перед парсингом
+        File(path).useLines { lines ->
+            lines.drop(1) // Пропускаем заголовок
+                .forEach { parsePlayerFromString(it) } // Парсим каждую строку
+        }
+        return players
+    }
+
+    private fun parsePlayerFromString(playerString: String) {
+        val playerParts = playerString.split(";")
+
+        players.add(
+            Player(
+                name = playerParts[0],
+                team = Team(name = playerParts[1], city = playerParts[2]),
+                position = Position.getByValue(playerParts[3]),
+                nationality = playerParts[4],
+                agency = playerParts[5],
+                transferCost = playerParts[6].toInt(),
+                participationCount = playerParts[7].toInt(),
+                goals = playerParts[8].toInt(),
+                assists = playerParts[9].toIntOrNull().orZero(),
+                yellowCards = playerParts[10].toIntOrNull().orZero(),
+                redCards = playerParts[11].toIntOrNull().orZero()
+            )
+        )
+    }
+
+    private fun Int?.orZero() = this ?: 0
+
+    // Функция для подсчета доли игроков из разных стран
+    fun getNationalityDistribution(players: List<Player>): Map<String, Double> {
+        val totalPlayers = players.size.toDouble()
+        return players.groupingBy { it.nationality }
+            .eachCount()
+            .mapValues { (_, count) -> (count / totalPlayers) * 100 }
+    }
+}
--- src/main/kotlin/resolver/Resolver.kt
+++ src/main/kotlin/resolver/Resolver.kt
@@ -1,3 +1,34 @@
 package resolver
 
-class Resolver
+import model.Player
+import model.Position
+import model.Team
+
+class Resolver (private val players: List<Player>) : IResolver {
+
+    // Выведите количество игроков, интересы которых не представляет агентство.
+    override fun getCountWithoutAgency(): Int = players.count { it.agency.isEmpty() }
+
+    // Выведите автора наибольшего числа голов из числа защитников и их количество.
+    override fun getBestScorerDefender(): Pair<String, Int> =
+        players
+            .filter { it.position == Position.DEFENDER }
+            .maxByOrNull { it.goals }
+            ?.let { it.name to it.goals }
+            ?: throw Exception("no defenders")
+
+    // Выведите русское название позиции самого дорогого немецкого игрока.
+    override fun getTheExpensiveGermanPlayerPosition(): String =
+        players
+            .filter { it.nationality == "Germany" }
+            .maxBy { it.transferCost }
+            .let { it.position.russianName }
+
+    // Выберите команду с наибольшим числом удалений на одного игрока.
+    override fun getTheRudestTeam(): Team =
+        players
+            .groupBy { it.team }
+            .maxBy { it.value.map { it.redCards }.average() }
+            .key
+
+}
