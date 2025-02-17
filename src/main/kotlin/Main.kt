0e2ff963937e972799489a425a504631d230a5b8
Almazova Vlada <almazova.2004@mail.ru>
2025-02-17 23:50:24 +0500

    пояснение ответа

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,10 +10,10 @@ fun main(args: Array<String>) {
 
     val resolver = Resolver(playerList, teamList)
 
-    println(resolver.getCountWithoutAgency())
-    println(resolver.getBestScorerDefender())
-    println(resolver.getTheExpensiveGermanPlayerPosition())
-    println(resolver.getTheRudestTeam().getDescription())
+    println("Количество игроков, интересы которых не представляет агенство:\n" + resolver.getCountWithoutAgency())
+    println("Автор наибольшего числа голов из числа защитников и их количество:\n" + resolver.getBestScorerDefender())
+    println("Русское название позиции самого дорогого немецкого игрока:\n" + resolver.getTheExpensiveGermanPlayerPosition())
+    println("Команда с наибольшим средним числом красных карточек на одного игрока:\n" + resolver.getTheRudestTeam().getDescription())
 
     GraphPlotter.createPieChart(resolver.getDistributionPositions())
 }
