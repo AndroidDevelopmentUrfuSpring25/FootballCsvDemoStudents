768073b01255d83a8ed58719449abcece50e2fc1
SAnya Amelin <sanyamelin7@gmail.com>
2025-02-17 16:49:35 +0500

    Домашнее задание "Основы Kotlin"

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,15 @@
+import model.Team
+import parser.CsvParser
+import resolver.Resolver
+
 fun main(args: Array<String>) {
-    print("Yeah rock!")
+    val players = CsvParser.parsePlayers("src/main/resources/fakePlayers.csv")
+    val resolver = Resolver(players)
+
+    println("Количество игроков без агентства: ${resolver.getCountWithoutAgency()}")
+    println("Лучший бомбардир среди защитников: ${resolver.getBestScorerDefender()}")
+    println("Самый дорогой немецкий игрок играет на позиции: ${resolver.getTheExpensiveGermanPlayerPosition()}")
+    println("Самая грубая команда: ${resolver.getTheRudestTeam().name}")
+
+    resolver.getTopTeamsByTransferCost("Амелин_Вариант_2.png")
 }
--- src/main/kotlin/parser/CsvParser.kt
+++ src/main/kotlin/parser/CsvParser.kt
@@ -1,3 +1,35 @@
 package parser
 
-object CsvParser
+import model.Player
+import java.io.File
+
+object CsvParser {
+    fun parsePlayers(filePath: String): List<Player> {
+        val lines = File(filePath).readLines()
+        val players = mutableListOf<Player>()
+
+        for (i in 1 until lines.size) {
+            val line = lines[i]
+            val tokens = line.split(";")
+
+            players.add(
+                Player(
+                    name = tokens[0],
+                    team = tokens[1],
+                    city = tokens[2],
+                    position = tokens[3],
+                    nationality = tokens[4],
+                    agency = if (tokens[5].isEmpty()) null else tokens[5],
+                    transferCost = tokens[6].toInt(),
+                    participations = tokens[7].toInt(),
+                    goals = tokens[8].toInt(),
+                    assists = tokens[9].toInt(),
+                    yellowCards = tokens[10].toInt(),
+                    redCards = tokens[11].toInt()
+                )
+            )
+        }
+
+        return players
+    }
+}
