e40d9571bcb9c319bc169f52ff388f03b5c76ac9
fadon1802 <pochta.antona1@gmail.com>
2025-02-18 01:52:37 +0500

    firstCommit

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,8 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     implementation("com.github.javafaker:javafaker:1.0.2")
+    implementation("com.opencsv:opencsv:5.7.1")
+    implementation("org.jfree:jfreechart:1.5.3")
 }
 
 tasks.test {
--- src/main/kotlin/model/Player.kt
+++ src/main/kotlin/model/Player.kt
@@ -1,3 +1,16 @@
 package model
 
-class Player
+data class Player(
+    val name: String?,
+    val team: String,
+    val city: String,
+    val position: String,
+    val nationality: String,
+    val agency: String,
+    val transferCost: String,
+    val participations: String,
+    val goals: String,
+    val assists: String,
+    val yellowCards: String,
+    val redCards: String,
+)
