6cb13f79c9e6e467cf9ce85b950ead323134b998
Soldatov Aleksey <Fotaxis@mail.ru>
2025-03-01 14:08:50 +0500

    Resolver methods fixes

--- src/main/kotlin/resolver/Resolver.kt
+++ src/main/kotlin/resolver/Resolver.kt
@@ -6,29 +6,26 @@ import model.Team
 
 class Resolver(private val players: List<Player>) : IResolver {
 
-    override fun getCountWithoutAgency(): Int {
-        return players.count { it.agency == "" }
-    }
+    override fun getCountWithoutAgency(): Int = players.count { it.agency.isBlank() }
 
-    override fun getBestScorerDefender(): Pair<String, Int> {
-        return players.filter { it.position == Position.DEFENDER }
+    override fun getBestScorerDefender(): Pair<String, Int> =
+        players
+            .filter { it.position == Position.DEFENDER }
             .maxByOrNull { it.goalsCount }
             ?.let { it.name to it.goalsCount }
             ?: throw Exception("Defenders not founded")
 
-    }
 
-    override fun getTheExpensiveGermanPlayerPosition(): String {
-        return players.filter { it.nationality == "Germany" }
+    override fun getTheExpensiveGermanPlayerPosition(): String =
+        players
+            .filter { it.nationality == "Germany" }
             .maxByOrNull { it.transferCost }?.position?.russianName
             ?: throw Exception("Germany player no founded")
-    }
 
-    override fun getTheRudestTeam(): Team {
-        return players
+    override fun getTheRudestTeam(): Team =
+        players
             .groupBy { it.team }
             .maxByOrNull { it.value.map { it.redCardsCount }.average() }
             ?.key
             ?: throw Exception("Empty players list")
-    }
 }
